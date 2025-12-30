542885225df35be0a3b5ed0ddb90cc1edc9ed479
Utkarsh Shukla <utkarshshukla372@gmail.com>
2025-12-30 20:36:03 +0530

    Fixes in deployment

--- backend-nodejs/server.js
+++ backend-nodejs/server.js
@@ -9,6 +9,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+//Ignore favicon requests
+app.get('/favicon.ico', (req, res) => res.status(204));
 // Middleware
 app.use(cors());
 app.use(express.json());
